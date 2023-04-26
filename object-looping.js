eedf7761093a6267dc75b1edf84a12df46a1f05f
Nasir Ahmed <ahsannasirahmed@gmail.com>
2023-04-26 12:58:29 +0600

    5

--- object-looping.js
+++ object-looping.js
@@ -1,4 +1,8 @@
-const myObject = { a: 1, b: 2, c: 3 };
+const myObject = { 
+  a: 1,
+   b: 2,
+    c: 3
+   };
 
 for (const property in myObject) {
   console.log(`${property}: ${myObject[property]}`);
